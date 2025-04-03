c37c937843def9e44151e2df80c120a77d685849
jampasfredirect <jampasfredirect@gmail.com>
2025-04-03 18:38:57 +0800

    add railway.app/register

--- db.php
+++ db.php
@@ -11,5 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}else {
+    echo "Database connection successful!";
 }
 ?>
