2fd5d54333e77feada9b0bf6c5d0d6a3d6c36da5
jampasfredirect <jampasfredirect@gmail.com>
2025-04-03 18:03:09 +0800

    add getenv

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$servername = "shuttle_proxy.rhyy.net";
-$port = 41167; // Added port number
-$username = "root";
-$password = "oYCEtxbowPQVorrpZukBtfryYPgPWMqZ";
-$dbname = "user_system";
+$servername = getenv('DB_SERVERNAME'); // Get the DB server from env
+$port = getenv('DB_PORT'); // Get the DB port from env
+$username = getenv('DB_USERNAME'); // Get the DB username from env
+$password = getenv('DB_PASSWORD'); // Get the DB password from env
+$dbname = getenv('DB_NAME'); // Get the DB name from env
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
